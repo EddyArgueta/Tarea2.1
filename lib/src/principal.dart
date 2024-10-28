3e6d6f29a07af13f7cd9ae9b8570f580ed77573f
CarlosArgueta <carguetar@unah.hn>
2024-10-28 15:08:36 -0600

    Dando forma

--- lib/src/principal.dart
+++ lib/src/principal.dart
@@ -8,12 +8,14 @@ class Principal extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        toolbarHeight: 10,
       ),
       body: ListView(
         padding: const EdgeInsets.all(16.0),
         children: [
           Card(
-            elevation: 5,
+            elevation: 0,
+            color: Colors.white30,
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(16.0),
             ),
@@ -24,7 +26,7 @@ class Principal extends StatelessWidget {
                   IconButton(
                     iconSize: 100,
                     icon: const Icon(
-                      Icons.face_unlock_rounded,
+                      Icons.tag_faces_outlined,
                       color: Colors.blue,
                     ),
                     onPressed: () {},
@@ -33,40 +35,60 @@ class Principal extends StatelessWidget {
                   const Text(
                     'Coffeestories',
                     style: TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
+                      fontSize: 30,
+                      fontWeight: FontWeight.w600,
                     ),
                   ),
                   const SizedBox(height: 8),
                   const Text(
                     'mark.brock@icloud.com',
                     style: TextStyle(
-                      fontSize: 16,
+                      fontSize: 14,
                     ),
                   ),
+
                   const SizedBox(height: 24),
                   ElevatedButton(
-                    onPressed: () {
-                      // Handle edit profile action
-                    },
+                    onPressed: () {},
                     child: const Text('Edit profile'),
                   ),
                 ],
               ),
             ),
           ),
-          const Divider(),
-          const CustomListTile(
-            icon: Icons.warehouse_outlined,
-            title: 'My stores',
-            trailing: Icons.arrow_forward,
+
+          const SizedBox(height: 24),
+          const Text(
+                      textAlign: TextAlign.start,
+                      'Inventories',
+                      style: TextStyle(
+                        fontSize: 16,
+                      ),
+                    ),
+          const Column(
+            children: [
+              CustomListTile(
+                icon: Icons.warehouse_outlined,
+                title: 'My stores',
+                trailing: Icons.arrow_forward,
+              ),
+              Divider(),
+              CustomListTile(
+                icon: Icons.support,
+                title: 'Support',
+                trailing: Icons.arrow_forward,
+              ),
+            ],
           ),
-          const CustomListTile(
-            icon: Icons.support,
-            title: 'Support',
-            trailing: Icons.arrow_forward,
-          ),
-          const Divider(),
+
+          const SizedBox(height: 24),
+          const Text(
+                      textAlign: TextAlign.start,
+                      'Preferences',
+                      style: TextStyle(
+                        fontSize: 16,
+                      ),
+                    ),
           const CustomListTile(
             icon: Icons.notifications_none_outlined,
             title: 'Push notifications',
@@ -82,10 +104,15 @@ class Principal extends StatelessWidget {
             title: 'PIN Code',
             trailing: Icons.arrow_forward,
           ),
-          const CustomListTile(
-            icon: Icons.logout_outlined,
-            title: 'Logout',
-            trailing: Icons.arrow_forward,
+          const Column(
+            children: [
+              ListTile(
+                      leading: Icon( Icons.logout_outlined ),
+                      title: Text('Logout'),
+                      iconColor: Colors.red,
+                      textColor: Colors.red, 
+                      ),
+            ],
           ),
         ],
       ),
